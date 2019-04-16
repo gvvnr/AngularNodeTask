fa912cc0cc0b715afe53df99225ab568f33a52c8
gvvnr <you@example.com>
2019-04-16 20:03:22 +0530

    commited

--- BuyProducts/src/app/app.module.ts
+++ BuyProducts/src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+//import {FilterPipeModule} from "ngx-filter-pipe";
+//import { FilterPipeModule } from 'ngx-filter-pipe'; // <-- Import
 
+const routes: Routes = [
+  { path: 'search', component: SearchComponent }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    //FilterPipeModule,
+    RouterModule.forRoot(routes)
   ],
+  exports: [ RouterModule ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
