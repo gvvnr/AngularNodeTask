454ba51866b2be3438bdedb15ac39f17254c5063
gvvnr <you@example.com>
2019-04-17 21:10:18 +0530

    commited

--- BuyProducts/src/app/app.module.ts
+++ BuyProducts/src/app/app.module.ts
@@ -1,29 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
-//import {FilterPipeModule} from "ngx-filter-pipe";
-//import { FilterPipeModule } from 'ngx-filter-pipe'; // <-- Import
+import { FilterPipeModule } from 'ngx-filter-pipe';
+import { DisplayItemsComponent } from './display-items/display-items.component';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { PaymentComponent } from './payment/payment.component';
+import { DisplayComponent } from './display/display.component';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
 
 const routes: Routes = [
-  { path: 'search', component: SearchComponent }
+  { path: 'search', component: SearchComponent },
+  { path: 'payment', component: PaymentComponent }
 ];
 @NgModule({
   declarations: [
     AppComponent,
-    SearchComponent
+    SearchComponent,
+    DisplayItemsComponent,
+    HeaderComponent,
+    FooterComponent,
+    PaymentComponent,
+    DisplayComponent,
+   // BrowserAnimationsModule
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    //FilterPipeModule,
-    RouterModule.forRoot(routes)
+    FilterPipeModule,
+    RouterModule.forRoot(routes),
+    FormsModule,
+    ReactiveFormsModule,
+    MatAutocompleteModule
   ],
   exports: [ RouterModule ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 
 export class AppModule { }
--- BuyProducts/src/app/search/search.component.ts
+++ BuyProducts/src/app/search/search.component.ts
@@ -1,15 +1,73 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Data} from './data';
+import {FormControl} from '@angular/forms';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.sass']
 })
 export class SearchComponent implements OnInit {
+  myControl = new FormControl();
+  options: string[] = ['One', 'Two', 'Three'];
+  //items: any[];
+  count :number[]=[1,2,3,4,5];
+  total :number;
+  totalData=new Array();
+ // userFilter: any;
+  value: any;
+  ItemCost: number;
+  data:any;
+  contains: any;
+  notContains: any;
+  //users: any[] = [{ name: 'John' }, { name: 'Jane                                                                                                                                                         ' }, { name: 'Mario' }];
+  //userFilter: any = { name: '' };
+  NameAndPrice: string;
+  selectedOption: number;
+  NameAndPriceValue=false;
+  ItemName:String;
+  items: Data[]=[
+    {name :'SOAP', cost:10},
+    {name :'Oil', cost:10},
+    {name :'Paste', cost:10},
+    {name :'Shampoo', cost:10},
+    {name :'CoolDrinks', cost:10},
 
-  constructor() { }
+  ];
+  checkOut(){
+    this.NameAndPrice=this.value+' '+this.total;
+    this.NameAndPriceValue=true;
+    this.totalData.push(this.NameAndPrice);
+    console.log(this.totalData);
+  }
+  totalCost() {
+    this.total=(this.selectedOption *this.ItemCost);
+  }
+  constructor() {
+
+  }
 
   ngOnInit() {
+
   }
+  itemNames(itemName){
+    this.value=itemName;
+    this.searchValue();
+  }
+
+  searchValue(){
+ let i;
+    for( i=0;i<this.items.length;i++){
+      if(this.value==this.items[i].name){
+        this.ItemCost=this.items[i].cost;
+        //.ItemName=this.value;
+        return this.contains=true;
+      }
+
+    }
+    if(i==this.items.length)
+      this.notContains=true;
+  }
+
+
 
 }
