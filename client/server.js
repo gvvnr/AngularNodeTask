694770b5b9ce257a158ff1de62fba4da892d816c
gvvnr <you@example.com>
2019-06-01 11:09:46 +0530

    commited

--- client/server.js
+++ client/server.js
@@ -5,6 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
+app.use(express.static("public"));
 app.use(express.static(__dirname + '/dist/buy-products'));
 
 app.get('/*', function(req,res) {
