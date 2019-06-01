b1b01dd2dfb71caacbf47f96b4edf2aba4d18665
gvvnr <you@example.com>
2019-06-01 10:43:46 +0530

    commited

--- client/server.js
+++ client/server.js
@@ -1,14 +1,16 @@
+//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-app.use(express.static('./dist/buy-products'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/buy-products'));
 
-app.get('/*' , (req , res) => {
-  res.sendFile(path.join(__dirname+'dist/buy-products/index.html'));
+app.get('/*', function(req,res) {
+
+  res.sendFile(path.join(__dirname+'/dist/buy-products/index.html'));
 });
 
-app.listen(process.env.PORT || 8080 ,() =>{
-  console.log('server started',process.env.PORT);
-});
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
